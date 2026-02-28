535d6f7d83e7c109cb1e83ed7e0c51e6c6c8c3c8
xiulixue <106227623+xiulixue@users.noreply.github.com>
2026-02-28 15:03:07 +1100

    setup: add github actions for auto deploy

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/React-Dev-Portfolio-Glassmorphism/',
 });
